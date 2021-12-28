7de3b0148dcf937f097a1f4a1b378e306d0ead4f
alaa <alaaahmed8383@gmail.com>
2021-12-28 13:31:10 +0400

    add addition function

--- UserManagement/Calculator/Addtion.cs
+++ UserManagement/Calculator/Addtion.cs
@@ -3,5 +3,11 @@ namespace UserManagement.Calculator
 {
     public class Addtion
     {
+        public static int Add(int NumberOne, int NumberTwo)
+        {
+            int Sum = 0;
+            Sum += NumberOne + NumberTwo;
+            return Sum;
+        }
     }
 }
