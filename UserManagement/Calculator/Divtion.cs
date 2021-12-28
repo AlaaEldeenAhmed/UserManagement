6c87fdc7cf39cd1c32ab188dba84639957d2c4c3
alaa <alaaahmed8383@gmail.com>
2021-12-28 13:33:53 +0400

    add divtion function

--- UserManagement/Calculator/Divtion.cs
+++ UserManagement/Calculator/Divtion.cs
@@ -2,6 +2,11 @@
 {
     public class Divtion
     {
-
+        public static float Div(int NumberOne, int NumberTwo)
+        {
+            int div = 0;
+            div = NumberOne / NumberTwo;
+            return div;
+        }
     }
 }
