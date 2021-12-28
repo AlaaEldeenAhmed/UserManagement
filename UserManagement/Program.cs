02cfa73f1af3ca7c7d2e9d3e698930db92eaf3d4
alaa <alaaahmed8383@gmail.com>
2021-12-28 14:12:12 +0400

    edit for release

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using UserManagement.Calculator;
+
+Console.WriteLine("------------User Management Calculation---------------------");
+Console.WriteLine("Addtion :" + Addtion.Add(5, 9));
+Console.WriteLine("Divtion :" + Divtion.Div(5, 9));
+Console.WriteLine("Modtion :" + Modtion.Mod(5, 9));
+Console.WriteLine("Multition :" + Multition.Mul(5, 9));
+Console.WriteLine("Subtion :" + Subtion.Sub(5, 9));
+Console.WriteLine("----------------End-----------------");
+
+Console.ReadKey();
