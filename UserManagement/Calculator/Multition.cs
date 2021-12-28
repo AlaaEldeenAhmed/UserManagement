56fb07ca9931f6eac4c3743624751cf113239bfe
alaa <alaaahmed8383@gmail.com>
2021-12-28 13:36:41 +0400

    add multiply function

--- UserManagement/Calculator/Multition.cs
+++ UserManagement/Calculator/Multition.cs
@@ -2,6 +2,11 @@
 {
     public class Multition
     {
-
+        public static int Mul(int NumberOne, int NumberTwo)
+        {
+            int mul;
+            mul = NumberOne * NumberTwo;
+            return mul;
+        }
     }
 }
