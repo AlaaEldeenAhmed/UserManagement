7ff8e3d76db8e5559e9c61628734e73f00607718
alaa <alaaahmed8383@gmail.com>
2021-12-28 13:45:11 +0400

    add modtion function

---
+++ UserManagement/Calculator/Modtion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.Calculator
+{
+    public class Modtion
+    {
+        public static float Mod(int NumberOne, int NumberTwo)
+        {
+            float mod = 0;
+            mod = NumberOne % NumberTwo;
+            return mod;
+        }
+    }
+}
